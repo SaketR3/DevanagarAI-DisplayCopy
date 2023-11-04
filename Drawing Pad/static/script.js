2a83cfd2812705854cb6d5670a1e9020c16f50c9
SaketR3 <saketr3@illinois.edu>
2023-11-03 21:15:03 -0500

    Added Drawing Pad Folder
    NOTE: The back-end is contained entirely in the folder called "Drawing Pad". None of the other folders are necessary to run the back-end.

---
+++ Drawing Pad/static/script.js
@@ -0,0 +1,146 @@
+var canvas = document.getElementById("paint");
+var ctx = canvas.getContext("2d");
+var width = canvas.width;
+var height = canvas.height;
+var curX, curY, prevX, prevY;
+var hold = false;
+ctx.lineWidth = 2;
+var fill_value = true;
+var stroke_value = false;
+var canvas_data = {"pencil": [], "eraser": []}
+                        
+function color(color_value){
+    ctx.strokeStyle = color_value;
+    ctx.fillStyle = color_value;
+}    
+        
+function add_pixel(){
+    ctx.lineWidth += 1;
+}
+        
+function reduce_pixel(){
+    if (ctx.lineWidth == 1){
+        ctx.lineWidth = 1;
+    }
+    else{
+        ctx.lineWidth -= 1;
+    }
+}
+        
+function fill(){
+    fill_value = true;
+    stroke_value = false;
+}
+        
+function outline(){
+    fill_value = false;
+    stroke_value = true;
+}
+               
+function reset(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    canvas_data = { "pencil": [], "line": [], "rectangle": [], "circle": [], "eraser": [] }
+}
+        
+// pencil tool
+        
+function pencil(){
+        
+    canvas.onmousedown = function(e){
+        curX = e.clientX - canvas.offsetLeft;
+        curY = e.clientY - canvas.offsetTop;
+        hold = true;
+            
+        prevX = curX;
+        prevY = curY;
+        ctx.beginPath();
+        ctx.moveTo(prevX, prevY);
+    };
+        
+    canvas.onmousemove = function(e){
+        if(hold){
+            curX = e.clientX - canvas.offsetLeft;
+            curY = e.clientY - canvas.offsetTop;
+            draw();
+        }
+    };
+        
+    canvas.onmouseup = function(e){
+        hold = false;
+    };
+        
+    canvas.onmouseout = function(e){
+        hold = false;
+    };
+        
+    function draw(){
+        ctx.lineTo(curX, curY);
+        ctx.stroke();
+        canvas_data.pencil.push({ "startx": prevX, "starty": prevY, "endx": curX, "endy": curY, "thick": ctx.lineWidth, "color": ctx.strokeStyle });
+    }
+}
+        
+// eraser tool
+        
+function eraser(){
+    
+    canvas.onmousedown = function(e){
+        curX = e.clientX - canvas.offsetLeft;
+        curY = e.clientY - canvas.offsetTop;
+        hold = true;
+            
+        prevX = curX;
+        prevY = curY;
+        ctx.beginPath();
+        ctx.moveTo(prevX, prevY);
+    };
+        
+    canvas.onmousemove = function(e){
+        if(hold){
+            curX = e.clientX - canvas.offsetLeft;
+            curY = e.clientY - canvas.offsetTop;
+            draw();
+        }
+    };
+        
+    canvas.onmouseup = function(e){
+        hold = false;
+    };
+        
+    canvas.onmouseout = function(e){
+        hold = false;
+    };
+        
+    function draw(){
+        ctx.lineTo(curX, curY);
+        ctx.strokeStyle = "#ffffff";
+        ctx.stroke();
+        canvas_data.pencil.push({ "startx": prevX, "starty": prevY, "endx": curX, "endy": curY, "thick": ctx.lineWidth, "color": ctx.strokeStyle });
+    }    
+}  
+
+document.getElementById('save').addEventListener('click', function(e) {
+    let canvasUrl = canvas.toDataURL("image/jpeg", 0.5);
+    console.log(canvasUrl);
+    const createEl = document.createElement('a');
+    createEl.href = canvasUrl;
+    createEl.download = "download-this-canvas";
+    createEl.click();
+    createEl.remove();
+  });
+
+function save(){
+    var filename = document.getElementById("fname").value;
+    var data = JSON.stringify(canvas_data);
+    var image = canvas.toDataURL("image/jpeg", .5);
+    console.log(canvasUrl);
+    const createEl = document.createElement('a');
+    createEl.href = image;
+    createEl.download = "download-this-canvas";
+    createEl.click();
+    document.body.appendChild(a);
+    createEl.remove();
+    
+    $.post("/", { save_fname: filename, save_cdata: data, save_image: image });
+    alert(filename + " saved");
+} 
