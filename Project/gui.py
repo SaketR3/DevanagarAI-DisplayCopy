5fe2eb03de858b2bd9d102497d5d31ac57251d27
aidanandrews22 <144747310+aidanandrews22@users.noreply.github.com>
2023-11-16 13:48:13 -0600

    Update gui.py

--- Project/gui.py
+++ Project/gui.py
@@ -4,25 +4,40 @@ import tkinter as tk
 from PIL import ImageGrab, Image
 import numpy as np
 
-model = load_model('mnist.h5')
 
-def predict_digit(img):
-    # Resize image to 28x28 pixels
-    img = img.resize((28, 28))
+# label possible outcomes
+devanagari_characters = [
+    "character_01_ka", "character_02_kha", "character_03_ga", "character_04_gha", "character_05_kna",
+    "character_06_cha", "character_07_chha", "character_08_ja", "character_09_jha", "character_10_yna",
+    "character_11_taamatar", "character_12_thaa", "character_13_daa", "character_14_dhaa", "character_15_adna",
+    "character_16_tabala", "character_17_tha", "character_18_da", "character_19_dha", "character_20_na",
+    "character_21_pa", "character_22_pha", "character_23_ba", "character_24_bha", "character_25_ma",
+    "character_26_yaw", "character_27_ra", "character_28_la", "character_29_waw", "character_30_motosaw",
+    "character_31_petchiryakha", "character_32_patalosaw", "character_33_ha", "character_34_chhya",
+    "character_35_tra", "character_36_gya", "digit_0", "digit_1", "digit_2", "digit_3",
+    "digit_4", "digit_5", "digit_6", "digit_7", "digit_8", "digit_9"
+]
+
+# Load the model
+model = load_model('aidan.h5')
+
+def predict_letter(img):
+    # Resize image to 32x32 pixels
+    img = img.resize((32, 32))
     # Convert rgb to grayscale
     img = img.convert('L')
-    # Invert the grayscale image to match MNIST data representation
+    # Invert the grayscale image to match data representation
     img = Image.fromarray(255 - np.array(img))
     img_array = np.array(img)
     # Reshaping to support our model input and normalizing
-    img_array = img_array.reshape(1, 28, 28, 1)
+    img_array = img_array.reshape(1, 32, 32, 1)
     img_array = img_array / 255.0
     # Predicting the class
     res = model.predict([img_array])[0]
     return np.argmax(res), max(res)
 
-# Displaying the modified predict_digit function
-predict_digit
+# Displaying the modified predict_letter function
+predict_letter
 
 class App(tk.Tk):
     def __init__(self):
@@ -32,7 +47,7 @@ class App(tk.Tk):
         
         # Creating elements
         self.canvas = tk.Canvas(self, width=300, height=300, bg = "white", cursor="cross")
-        self.label = tk.Label(self, text="Draw..", font=("Helvetica", 48))
+        self.label = tk.Label(self, text="Draw a Devanagari character..", font=("Helvetica", 48))
         self.classify_btn = tk.Button(self, text = "Recognise", command = self.classify_handwriting)   
         self.button_clear = tk.Button(self, text = "Clear", command = self.clear_all)
        
@@ -54,9 +69,9 @@ class App(tk.Tk):
         x1 = x0 + self.canvas.winfo_width() - 8
         y1 = y0 + self.canvas.winfo_height() - 8
         im = ImageGrab.grab((x0, y0, x1, y1))
-
-        digit, acc = predict_digit(im)
-        self.label.configure(text= str(digit)+', '+ str(int(acc*100))+'%')
+        
+        letter, acc = predict_letter(im)
+        self.label.configure(text= "{} - {:.2f}%".format(devanagari_characters[letter], acc*100))
 
     def draw_lines(self, event):
         self.x = event.x
