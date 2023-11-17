16e58629d9c73ae3644a684fefed00548884d63b
aidanandrews22 <144747310+aidanandrews22@users.noreply.github.com>
2023-11-17 16:03:45 -0600

    Update tutorial.py

--- Project/tutorial.py
+++ Project/tutorial.py
@@ -1,69 +1,129 @@
-import keras
-from keras.datasets import mnist
+import numpy as np
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelBinarizer
+
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
-from keras import backend as K
-from keras.layers import BatchNormalization    
-from keras.callbacks import ReduceLROnPlateau     
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D, BatchNormalization, Activation
+from keras.optimizers import Adam
+from keras.callbacks import LearningRateScheduler, ReduceLROnPlateau
+from keras.preprocessing.image import ImageDataGenerator
 
-# the data, split between train and test sets
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-print(x_train.shape, y_train.shape)
+import matplotlib.pyplot as plt
 
-x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
-x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
-input_shape = (28, 28, 1)
 
-# convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, 10)
-y_test = keras.utils.to_categorical(y_test, 10)
+# Load the data
+train = pd.read_csv("data/data.csv")
+X = train.drop("character", axis=1)
+y0 = train["character"]
+binencoder = LabelBinarizer()
+y = binencoder.fit_transform(y0)
 
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
-print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
+# Split the data into train and test sets
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=90)
+x_train = x_train / 255
+x_test = x_test / 255
 
-batch_size = 128
-num_classes = 10
-epochs = 10
+# Reshape the data
+re_x_train = x_train.values.reshape(-1, 32, 32, 1)
+re_x_test = x_test.values.reshape(-1, 32, 32, 1)
 
-# Model architecture
+# Create an ImageDataGenerator object for data augmentation
+datagen = ImageDataGenerator(
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    zoom_range=[0.7, 1.3],
+    shear_range=0.1,
+    fill_mode='nearest'
+)
+
+# Define a learning rate schedule
+def scheduler(epoch, lr):
+    if epoch < 10:
+        return lr
+    else:
+        return lr * 0.9  # Example: reduce lr by 10% every epoch after 10
+    
+lr_scheduler = LearningRateScheduler(scheduler)
+
+# Reduce learning rate of plateau
+lr_plateau_callback = ReduceLROnPlateau(
+    monitor='val_loss',
+    factor=0.5,
+    patience=3,
+    verbose=1
+)
+
+# Enhanced Model Architecture
 model = Sequential()
-model.add(Conv2D(32, kernel_size=(5, 5),activation='relu',input_shape=input_shape))
+
+# Increasing depth and filters
+model.add(Conv2D(64, (3,3), padding='same', input_shape=(32, 32, 1), activation='relu'))
 model.add(BatchNormalization())
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64, (3,3), padding='same', activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
 
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True, beta_initializer="zeros", gamma_initializer="ones", moving_mean_initializer="zeros", moving_variance_initializer="ones", beta_regularizer=None, gamma_regularizer=None, beta_constraint=None, gamma_constraint=None, synchronized=False))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(128, (3,3), padding='same', activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
 
-model.add(Conv2D(128, (3, 3), activation='relu')) # Added another convolutional layer
-model.add(BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True, beta_initializer="zeros", gamma_initializer="ones", moving_mean_initializer="zeros", moving_variance_initializer="ones", beta_regularizer=None, gamma_regularizer=None, beta_constraint=None, gamma_constraint=None, synchronized=False))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(256, (3,3), padding='same', activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+# Additional layers for complexity
+model.add(Conv2D(512, (3,3), padding='same', activation='relu'))
+model.add(BatchNormalization())
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
 
 model.add(Flatten())
-model.add(Dense(256, activation='relu')) # Increased neurons
-model.add(Dropout(0.3))
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.4)) # Adjusted dropout
-model.add(Dense(num_classes, activation='softmax'))
+model.add(Dense(512, activation='relu', kernel_regularizer='l2'))
+model.add(Dropout(0.5))
+model.add(Dense(46, activation='softmax'))  # Assuming 46 classes for different characters
 
-# Using Adam optimizer
-model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(lr=0.001), metrics=['accuracy'])
+model.summary()
 
-reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, min_lr=0.001)
+# Compile the model
+optimizer = Adam(lr=0.001)
+model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
 
-hist = model.fit(x_train, y_train, batch_size=128, epochs=10, verbose=1, validation_data=(x_test, y_test), callbacks=[reduce_lr])
-print("The model has successfully trained")
+# Training with data augmentation
+history = model.fit(
+    datagen.flow(re_x_train, y_train, batch_size=32),
+    validation_data=(re_x_test, y_test),
+    epochs=150,  # Increased epochs
+    verbose=2,
+    callbacks=[lr_scheduler, lr_plateau_callback]
+)
 
-score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
 
-model.save('mnist.h5')
-print("Saving the model as mnist.h5")
+print(re_x_train.shape)
+print(re_x_test.shape)
+print(y_train.shape)
+print(y_test.shape)
+
+
+# Evaluate the model
+model.evaluate(re_x_test, y_test, verbose=2)
+
+# Plot the accuracy and loss
+history_df = pd.DataFrame(history.history)
+history_df.loc[:, ['loss', 'val_loss', 'accuracy', 'val_accuracy']].plot()
+
+# Plot the confusion matrix
+X_images = X.values.reshape((-1,32,32))
+
+plt.imshow(X_images[555])
+plt.show()
+
+imgTrans = X_images[555].reshape(1,32,32,1)
+predictions = model.predict(imgTrans)
+binencoder.classes_[np.argmax(predictions)]
+
+model.save('aidan.h5')
+print("Saving the model as aidan.h5")
